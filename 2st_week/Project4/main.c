81c47f1b08e86d0ae630676172559a7b4906c774
LimHanseong <hanseong2049@gmail.com>
2022-09-21 17:02:24 +0900

    2st_week

--- 2st_week/Project4/main.c
+++ 2st_week/Project4/main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main(void)
 {
-	double weight, height, bmi;
+	double weight, height, bmi;						//몸무게, 키, bmi 실수형 동시 선언
 
-	printf("몸무게(kg)와 키(cm) 입력 : ");
-	scanf_s("%lf%lf", &weight, &height);
+	printf("몸무게(kg)와 키(cm) 입력 : ");			//몸무게와 키 입력 메세지 출력
+	scanf_s("%lf%lf", &weight, &height);			//%lf로 실수형 몸무게와 키 입력, 변수 앞에 &로 지정
 
-	height = height / 100;
-	bmi = weight / (height * height);
+	height = height / 100;							//키 m단위 환산
+	bmi = weight / (height * height);				//bmi 계산식
 
-	((bmi >= 20.0) && (bmi < 25.0))
-		? printf("표준입니다.\n")
-		: printf("체중관리가 필요합니다.\n");
+	((bmi >= 20.0) && (bmi < 25.0))					//20이상 25미만의 두 범위 논리곱 연산자
+		? printf("표준입니다.\n")					//? 두 범위가 모두 참일 때, 표준입니다 출력
+		: printf("체중관리가 필요합니다.\n");		//: 두 범위 모두 중 하나라도 거짓일 때 체중관리가 필요합니다 출력
 
 	return 0;
 }
--- 2st_week/Project4if절/main.c
+++ 2st_week/Project4if절/main.c
@@ -9,13 +9,13 @@ int main(void)
 	height = height / 100;
 	bmi = weight / (height * height);
 
-	if (20.0 <= bmi < 25.0)
+	if (20.0 <= bmi < 25.0)						//if~else절을 사용하여 bmi의 범위를 정해줌.
 	{
-		printf("표준입니다.\n");
+		printf("표준입니다.\n");				//bmi가 범위 안의 값을 때 표준입니다 출력 후 줄바꿈
 	}
 	else
 	{
-		printf("체중관리가 필요합니다.\n");
+		printf("체중관리가 필요합니다.\n");		//bmi가 범위 안의 값이 아닐 때 체중관리가 필요합니다 출력 후 줄바꿈
 	}
 
 	return 0;
