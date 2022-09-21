48e241d000eeaee4439ea4fd52d8682d77ce170c
LimHanseong <hanseong2049@gmail.com>
2022-09-21 16:18:24 +0900

    1st_week/HellowWord

--- 1st_week/HellowWord/main.c
+++ 1st_week/HellowWord/main.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
-int main(void)
+//1st_week project
+#include <stdio.h>					//지정한 파일을 추가하는 지시자
+int main(void)						//main 함수
 {
-	printf("Hellow World!\n");
-	return 0;
+	printf("Hellow World!\n");		//Hellow World! 문자열을 출력 후\n으로 줄 바꿈 
+	return 0;						//프로그램 종료
 }
